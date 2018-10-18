179ac2084563b98bf465b7b65f1d124be0dc9faa
MeleshkoAlex <wertigo11111@gmail.com>
2018-10-18 21:56:18 +0300

    add typing-speed

---
+++ typing-speed/user.js
@@ -0,0 +1,119 @@
+;(function(){
+	var arrTextRu = [
+	'Негде, в тридевятом царстве, В тридесятом государстве, Жил-был славный царь Дадон. Смолоду был грозен он И соседям то и дело Наносил обиды смело; Но под старость захотел Отдохнуть от ратных дел И покой себе устроить. Тут соседи беспокоить Стали старого царя, Страшный вред ему творя. Чтоб концы своих владений Охранять от нападений, Должен был он содержать Многочисленную рать. Воеводы не дремали, Но никак не успевали. Ждут, бывало, с юга, глядь, - Ан с востока лезет рать! Справят здесь, - лихие гости Идут от моря... Со злости Инда плакал царь Дадон, Инда забывал и сон. Что и жизнь в такой тревоге! Вот он с просьбой о помоге Обратился к мудрецу, Звездочёту и скопцу. Шлёт за ним гонца с поклоном.','Вот мудрец перед Дадоном Стал и вынул из мешка Золотого петушка. “Посади ты эту, птицу, - Молвил он царю,- на спицу; Петушок мой золотой Будет верный сторож твой: Коль кругом всё будет мирно, Так сидеть он будет смирно; Но лишь чуть со стороны Ожидать тебе войны, Иль набега силы бранной, Иль другой беды незваной Вмиг тогда мой петушок Приподымет гребешок, Закричит и встрепенётся И в то место обернётся”. Царь скопца благодарит, Горы золота сулит. “За такое одолженье, - Говорит он в восхищенье, - Волю первую твою Я исполню, как мою”.','Петушок с высокой спицы Стал стеречь его границы. Чуть опасность где видна, Верный сторож как со сна Шевельнётся, встрепенётся, К той сторонке обернётся И кричит: “Кири-ку-ку. Царствуй, лёжа на боку!” И соседи присмирели, Воевать уже не смели: Таковой им царь Дадон Дал отпор со всех сторон! Год, другой проходит мирно; Петушок сидит всё смирно. Вот однажды царь Дадон Страшным шумом пробуждён: “Царь ты наш! отец народа! - Возглашает воевода. - Государь! проснись! беда!” - “Что такое, господа? - Говорит Дадон, зевая, - А?.. Кто там?.. беда какая?” Воевода говорит: “Петушок опять кричит; Страх и шум во всей столице”. Царь к окошку, - ан на спице, Видит, бьётся петушок, Обратившись на восток. Медлить нечего: “Скорее! Люди, на конь! Эй, живее!” Царь к востоку войско шлёт, Старший сын его ведёт. Петушок угомонился, Шум утих, и царь забылся.','Вот проходит восемь дней, А от войска нет вестей; Было ль, не было ль сраженья, - Нет Дадону донесенья. Петушок кричит опять; Кличет царь другую рать; Сына он теперь меньшого Шлёт на выручку большого. Петушок опять утих. Снова вести нет от них! Снова восемь дней проходят; Люди в страхе дни проводят; Петушок кричит опять; Царь скликает третью рать И ведёт её к востоку, - Сам, не зная, быть ли проку. Войска идут день и ночь; Им становится невмочь. Ни побоища, ни стана, Ни надгробного кургана Не встречает царь Дадон. “Что за чудо?” - мыслит он. Вот осьмой уж день проходит, Войско в горы царь приводит И промеж высоких гор Видит шёлковый шатёр. Всё в безмолвии чудесном Вкруг шатра; в ущелье тесном Рать побитая лежит. Царь Дадон к шатру спешит... Что за страшная картина! Перед ним его два сына Без шеломов и без лат Оба мёртвые лежат,','Меч вонзивши друг во друга. Бродят кони их средь луга По притоптанной траве, По кровавой мураве... Царь завыл: “Ох, дети, дети! Горе мне! попались в сети Оба наши сокола! Горе! смерть моя пришла”. Все завыли за Дадоном, Застонала тяжким стоном Глубь долин, и сердце гор Потряслося. Вдруг шатёр Распахнулся... и девица, Шамаханская царица, Вся сияя как заря, Тихо встретила царя. Как пред солнцем птица ночи, Царь умолк, ей глядя в очи, И забыл он перед ней Смерть обоих сыновей. И она перед Дадоном Улыбнулась - и с поклоном Его за руку взяла И в шатёр свой увела. Там за стол его сажала, Всяким яством угощала; Уложила отдыхать На парчовую кровать, И потом, неделю ровно, Покорясь ей безусловно, Околдован, восхищён, Пировал у ней Дадон.','Наконец и в путь обратный Со своею силой ратной И с девицей молодой Царь отправился домой. Перед ним молва бежала, Быль и небыль разглашала. Под столицей, близ ворот, С шумом встретил их народ, - Все бегут за колесницей, За Дадоном и царицей; Всех приветствует Дадон... Вдруг в толпе увидел он, В сарачинской шапке белой, Весь как лебедь поседелый, Старый друг его, скопец. “А! здорово, мой отец, - Молвил царь ему, - что скажешь? Подь поближе! Что прикажешь?” - - Царь! - ответствует мудрец, - Разочтёмся наконец, Помнишь? за мою услугу Обещался мне, как другу, Волю первую мою Ты исполнить, как свою. Подари ж ты мне девицу. - Шамаханскую царицу... - Крайне царь был изумлён. “Что ты? - старцу молвил он, - Или бес в тебя ввернулся? Или ты с ума рехнулся? Что ты в голову забрал?','Я, конечно, обещал, Но всему же есть граница! И зачем тебе девица? Полно, знаешь ли, кто я? Попроси ты от меня Хоть казну, хоть чин боярский, Хоть коня с конюшни царской, Хоть полцарства моего”. - Не хочу я ничего! Подари ты мне девицу, Шамаханскую царицу, - Говорит мудрец в ответ. Плюнул царь: “Так лих же: нет! Ничего ты не получишь. Сам себя ты, грешник, мучишь; Убирайся, цел пока; Оттащите старика!” Старичок хотел заспорить, Но с иным накладно вздорить; Царь хватил его жезлом','По лбу; тот упал ничком, Да и дух вон. - Вся столица Содрогнулась; а девица - Хи-хи-хи! да ха-ха-ха! Не боится, знать, греха. Царь, хоть был встревожен сильно, Усмехнулся ей умильно. Вот - въезжает в город он... Вдруг раздался лёгкий звон, И в глазах у всей столицы Петушок спорхнул со спицы; К колеснице полетел И царю на темя сел, Встрепенулся, клюнул в темя И взвился... и в то же время С колесницы пал Дадон - Охнул раз, - и умер он. А царица вдруг пропала, Будто вовсе не бывало. Сказка ложь, да в ней намёк! Добрым молодцам урок.'];
+
+	var arrTextEn = ['Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat! Lorem ipsum dolor sit amet, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat! Et harum quidem rerum facilis est et expedita distinctio, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui...',
+	'Ut enim ad minim veniam, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, ut et voluptates repudiandae sint et molestiae non recusandae! Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minima veniam, nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, nisi ut aliquid ex ea commodi consequatur. Excepteur...',
+	'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, velit esse cillum dolore eu fugiat nulla pariatur! Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, consectetur adipiscing elit, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Sed ut perspiciatis, consectetur adipiscing elit, ut et voluptates repudiandae sint et molestiae non recusandae. Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, nisi ut aliquid ex ea commodi consequatur! Itaque earum rerum hic tenetur a sapiente delectus, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, sed...',
+	'Sed ut perspiciatis, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Ut enim ad minima veniam, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo? Lorem ipsum dolor sit amet, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, ut aut reiciendis voluptatibus maiores alias...',
+	'Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, qui dolorem eum fugiat, quo voluptas nulla pariatur. At vero eos et accusamus et iusto odio dignissimos ducimus, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua! Itaque earum rerum hic tenetur a sapiente delectus, nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Sed ut perspiciatis, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, sed do eiusmod tempor incididunt...',
+	'At vero eos et accusamus et iusto odio dignissimos ducimus, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, velit esse cillum dolore eu fugiat nulla pariatur. Itaque earum rerum hic tenetur a sapiente delectus, quis nostrum exercitationem ullam corporis suscipit laboriosam, velit esse cillum dolore eu fugiat nulla pariatur! At vero eos et accusamus et iusto odio dignissimos ducimus, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Itaque earum rerum hic tenetur a sapiente delectus, unde omnis...',
+	'Ut enim ad minima veniam, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, ut et voluptates repudiandae sint et molestiae non recusandae! Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, nisi ut aliquid ex ea commodi consequatur. Et harum quidem rerum facilis est et expedita distinctio, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque...',
+	'Quis autem vel eum iure reprehenderit, quis nostrum exercitationem ullam corporis suscipit laboriosam, velit esse cillum dolore eu fugiat nulla pariatur. Quis autem vel eum iure reprehenderit, quis nostrum exercitationem ullam corporis suscipit laboriosam, velit esse cillum dolore eu fugiat nulla pariatur. Duis aute irure dolor in reprehenderit in voluptate, nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et harum quidem rerum facilis est et expedita distinctio, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora...',
+	'Lorem ipsum dolor sit amet, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, sunt in culpa qui officia deserunt mollit anim id est laborum? Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, sunt in culpa qui officia deserunt mollit anim id est laborum! Excepteur sint occaecat cupidatat non proident, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur! Nemo enim ipsam voluptatem, nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, nisi ut aliquid ex ea commodi consequatur. Duis aute irure dolor in reprehenderit...',
+	'Excepteur sint occaecat cupidatat non proident, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, nisi ut aliquid ex ea commodi consequatur. Itaque earum rerum hic tenetur a sapiente delectus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem? Nemo enim ipsam voluptatem, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, ut et voluptates repudiandae sint et molestiae non recusandae. At vero eos et accusamus et iusto odio dignissimos ducimus, nam...'];
+
+	var lang = document.querySelector('#lang');
+	var text = document.querySelector('.text');
+	var butStart = document.querySelector('.start-but');
+	var inputDiv = document.querySelector('.input-div');
+	var timerBlock = document.querySelector('.timer');
+	var result = document.querySelector('.result');
+	var werp = document.querySelector('.werp');
+	butStart.addEventListener('click',start);
+	document.addEventListener('contextmenu', function(event){
+		event.preventDefault();
+	});
+	inputDiv.addEventListener('keypress',validOpt);
+	var arrLen = null;
+	var lengvid = null;
+	function start(){
+		inputDiv.focus();
+		timer();
+		inputDiv.innerHTML = "";
+
+		if(lang.selectedIndex == 0){
+			arrLen = randomInteger(0,arrTextRu.length-1); 
+			text.innerHTML = arrTextRu[arrLen];
+			lengvid = 0;
+		}
+		if(lang.selectedIndex == 1){
+			arrLen = randomInteger(0,arrTextEn.length-1); 
+			text.innerHTML = arrTextEn[arrLen];
+			lengvid = 1;
+		}
+	}
+	var delay = 3;
+	function timer(){
+		disabledBut();
+		var timerDelay = setTimeout(function(){
+			if(delay == 0){
+				timeLine();
+				clearTimeout(timerDelay);
+				return;
+			}
+			timerBlock.innerHTML = 'начало через '+ delay +' сек';
+			delay--;
+			timer();
+		},1000);
+	}
+	var counter = 60;
+	var timers;
+	function timeLine(){
+		 	timers = setTimeout(function(){
+			timerBlock.innerHTML = 'осталось - '+counter + ' сек';
+			if(counter == 0){
+				clearTimeout(timers);
+				remdisabledBut();
+				result.innerHTML = Math.round(inputDiv.innerHTML.length) + ' - знаков в минуту'; 
+				return;
+			}
+			counter--;
+			timeLine();
+		},1000);  
+	}
+	function disabledBut(){
+		butStart.setAttribute('disabled', '');
+	}
+	function remdisabledBut(){
+		butStart.removeAttribute('disabled');
+	}
+	function randomInteger(min, max) {
+		var rand = min + Math.random() * (max + 1 - min);
+		rand = Math.floor(rand);
+		return rand;
+	}
+	var count = 0;
+	function validOpt(event){
+		var key = event.key;
+		var even = event;
+		if(lengvid == 0){
+			 return validText(arrTextRu[arrLen],key,even);
+		}
+		if(lengvid == 1){
+			validText(arrTextEn[arrLen],key,even);
+		}
+		return false;
+	}
+	function validText(arr,key,even){
+		console.log('count в начале вызова ' + count);
+		if(arr[count] == key){
+
+			count++;
+			console.log('след = '+count,);
+		}else{
+			werp.classList.add('error');
+			var error = setTimeout(function(){
+				werp.classList.remove('error');
+			},50);
+			console.log('с чем было сравненеие ' +arr[count] );
+			count = inputDiv.innerHTML.length;
+			event.preventDefault();
+			console.log('клавиша = '+key, 'вернуло '+ count, 'длина строки ' + inputDiv.innerHTML.length);
+			 if(inputDiv.innerHTML.indexOf('&nbsp;') != -1){
+			 	count = inputDiv.innerHTML.length - 5;
+			 	console.log('нашел пробел');
+			}
+		}
+	}
+})();
